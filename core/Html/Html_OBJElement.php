b95ff701f7d13091a270c92fc6015292bd95286c
Salvatore Gonda <35118923+deemount@users.noreply.github.com>
2018-05-29 14:19:23 +0200

    Update Html_OBJElement.php

--- core/Html/Html_OBJElement.php
+++ core/Html/Html_OBJElement.php
@@ -10,7 +10,7 @@ namespace Dmount\Core\Html;
 use Dmount\Core\Html\ {
 	Internal\ElementsLibrary as ELib,
 	Internal\AttributesLibrary as ALib,
-    Internal\ValuesLibrary as VLib,
+        Internal\ValuesLibrary as VLib,
 	Internal\PutElementsObject as Putter
 };
 
@@ -94,44 +94,38 @@ class Html_OBJElement {
   public function register()
   {
 	
-	$class  = '';
-	$handle = '';
-	$file   = '';
-	$subDir = array('.', '..','Void','Internal','Embed','Root','Meta','Script','Sections','Group','Text','Modification','Table','Form','Interactive');
+    $class  = '';
+    $handle = '';
+    $file   = '';
+    $subDir = array('.', '..','Void','Internal','Embed','Root','Meta','Script','Sections','Group','Text','Modification','Table','Form','Interactive');
 	
-	$this->listElementsCategoryName();
-
+    $this->listElementsCategoryName();
     if($this->putElementsDirFile()) 
-	{
-	  
-	  foreach($this->el as $elDir => $elFiles)
-	  {
-		$elDir = ucfirst($elDir);
-		if($elDir !== 'Void')
-		{
-		  
+    {  
+      foreach($this->el as $elDir => $elFiles)
+      {
+        $elDir = ucfirst($elDir);
+	if($elDir !== 'Void')
+	{	  
           $handle = opendir($this->dir.$elDir); 
           while(false !== ($file = readdir($handle)))
-          {
-			  
-		    if(!in_array($file,$subDir))
-		    {
-			  if(file_exists($this->dir.$elDir.'/'.$file))
-			  {
-		        $element                     = str_replace('.php','',$file);
-			    $class                       = __NAMESPACE__.'\Elements\\'.$elDir.'\\'.$element;
-                $this->obj[$elDir][$element] = $class;
-			  }
-		    }
-			
-          }
-	      closedir($handle);
-		}
-	  }
-	  
+          {		  
+            if(!in_array($file,$subDir))
+            {
+	      if(file_exists($this->dir.$elDir.'/'.$file))
+	      {
+	        $element                     = str_replace('.php','',$file);
+	        $class                       = __NAMESPACE__.'\Elements\\'.$elDir.'\\'.$element;
+	        $this->obj[$elDir][$element] = $class;
+	      }
+	    }		
+          }//Eof while
+	  closedir($handle);
+	}
+      }//Eof foreach
     }
-	ksort($this->obj,SORT_STRING);
-	return $this;
+    ksort($this->obj,SORT_STRING);
+    return $this;
 	
   }//Eof Method "register"
 
@@ -145,14 +139,15 @@ class Html_OBJElement {
   {
 
     foreach($this->obj as $subDir => $file)
-	{
-	  foreach($file as $filename => $classname)
-	  {
-	    require_once $this->dir.$subDir.'/'.$filename.'.php';
-	    spl_autoload($classname);
-	  }
-	}
-	return $this;
+    {
+      foreach($file as $filename => $classname)
+      {
+        require_once $this->dir.$subDir.'/'.$filename.'.php';
+	spl_autoload($classname);
+      }
+    }
+    
+    return $this;
 	
   }//Eof Method "load"
 
@@ -165,38 +160,38 @@ class Html_OBJElement {
   public function initialize()
   {
 
-	foreach($this->obj as $dir => $file)
-	{
-	  foreach($file as $filename => $classname)
-	  {
+    foreach($this->obj as $dir => $file)
+    {
+      foreach($file as $filename => $classname)
+      {
 
-	    /*
-		* @desc 
-		*/
-		$element                   = str_replace($this->prefixClass.$dir.'_','',$filename);
-		$element                   = strtolower($element);
-	    $this->obj[$dir][$element] = new $classname;
-		
-		/*
-		* @desc 
-		*/
-		$methods = get_class_methods($classname);
-        foreach($methods as $methodName) 
-		{
-		  if($methodName !== '__construct')
-		  {
-			$prepMethodName = strtolower(str_replace('__setAttr_','',$methodName));
-			$arrMethodName  = explode('_',$prepMethodName);
-			if(count($arrMethodName)>1)
-			{
-              $this->val[$dir][$element][$arrMethodName[0]][$arrMethodName[1]] = call_user_func(array($this->obj[$dir][$element],$methodName));
-			}
-		  }
-        }
-		unset($this->obj[$dir][$filename]);
-	  }
-	}
-	//return $this;
+        /*
+    	* @desc 
+    	*/
+    	$element                   = str_replace($this->prefixClass.$dir.'_','',$filename);
+    	$element                   = strtolower($element);
+    	$this->obj[$dir][$element] = new $classname;
+
+    	/*
+    	* @desc 
+    	*/
+    	$methods = get_class_methods($classname);
+    	foreach($methods as $methodName) 
+    	{
+      	  if($methodName !== '__construct')
+      	  {
+	    $prepMethodName = strtolower(str_replace('__setAttr_','',$methodName));
+	    $arrMethodName  = explode('_',$prepMethodName);
+	    if(count($arrMethodName)>1)
+	    {
+	      $this->val[$dir][$element][$arrMethodName[0]][$arrMethodName[1]] = call_user_func(array($this->obj[$dir][$element],$methodName));
+	    }
+          }
+        }//Eof foreach
+       unset($this->obj[$dir][$filename]);
+     }
+    }
+    //return $this;
 	  
   }//Eof Method "initialize"
 
@@ -209,8 +204,8 @@ class Html_OBJElement {
   public static function requireOnce(string $name=NULL)
   {
     
-	$this->obj[] = ucfirst($name);
-	require_once $this->dir.$this->obj.'.php';
+    $this->obj[] = ucfirst($name);
+    require_once $this->dir.$this->obj.'.php';
 	
   }//Eof Method "get"
 
@@ -223,11 +218,11 @@ class Html_OBJElement {
   public static function set(array $args=NULL) 
   {
 	
-	$name = __NAMESPACE__.'\Elements\\'.$this->prefixClass.$this->obj;
-	call_user_func(array($name, 'setAttributes'), $args);
+    $name = __NAMESPACE__.'\Elements\\'.$this->prefixClass.$this->obj;
+    call_user_func(array($name, 'setAttributes'), $args);
     $str = call_user_func(array($name, 'setContainer'));
 	
-	return $str;
+    return $str;
 	
   }//Eof Method "set"
 
@@ -240,7 +235,7 @@ class Html_OBJElement {
   protected function setElementsPath()
   {
 	  
-	$this->dir = CORE.str_replace(array(__NAMESPACE__.'\Html','OBJ','_'),array('Html','','/'),get_class($this)).'s/';
+    $this->dir = CORE.str_replace(array(__NAMESPACE__.'\Html','OBJ','_'),array('Html','','/'),get_class($this)).'s/';
 	
   }//Eof Method "setElementPath"
 
